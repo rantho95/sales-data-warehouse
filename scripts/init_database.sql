6776db0e04b87e336f1af402cd6c3fd1a468a194
rantho95 <sehlabanerantho@gmail.com>
2026-08-30 00:53:43 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -44,6 +44,19 @@ GO
 USE DataWarehouse;
 GO
 
+/*
+=========================================================================================
+ DDL STAGE: CREATE ALL TABLES
+=========================================================================================
+*/
+
+USE DataWarehouse;
+GO
+-- Create staging Table
+PRINT'>> Droping Table: Staging.SalesRaw'
+IF OBJECT_ID('Staging.SalesRaw', 'U') IS NOT NULL
+	DROP TABLE Staging.SalesRaw;
+
 CREATE TABLE Staging.SalesRaw (
     ProductID       NVARCHAR(10),
     [Date]          NVARCHAR(30),
@@ -68,6 +81,10 @@ CREATE TABLE Staging.SalesRaw (
 GO
 
 --Create Date table
+PRINT'>> Droping Table: dim.DimDate'
+IF OBJECT_ID('dim.DimDate', 'U') IS NOT NULL
+	DROP TABLE dim.DimDate;
+
 CREATE TABLE dim.DimDate (
     DateKey       INT NOT NULL PRIMARY KEY,   -- smart key, format YYYYMMDD
     [Date]        DATE  NULL,
@@ -82,33 +99,43 @@ CREATE TABLE dim.DimDate (
 );
 GO
 
-
-
 -- Create DimProduct Table
+PRINT'>> Droping Table: dim.DimProduct'
+IF OBJECT_ID('dim.DimProduct ', 'U') IS NOT NULL
+	DROP TABLE dim.DimProduct;
+
 CREATE TABLE dim.DimProduct (
 	ProductKey INT IDENTITY(1,1) PRIMARY KEY,
-  ProductID  NVARCHAR(10) NOT NULL UNIQUE,
-  Product    NVARCHAR(100) NULL,
-  Category   NVARCHAR(30) NULL,
-  Segment    NVARCHAR(30) NULL,
+    ProductID  NVARCHAR(10) NOT NULL UNIQUE,
+    Product    NVARCHAR(100) NULL,
+    Category   NVARCHAR(30) NULL,
+    Segment    NVARCHAR(30) NULL,
 	UnitCost   DECIMAL(10,2) NULL,
 	UnitPrice  DECIMAL(10,2)  NULL
 );
 GO
 
 -- Create DimCustomer Table
+PRINT'>> Droping Table: dim.DimCustomer'
+IF OBJECT_ID('dim.DimCustomer ', 'U') IS NOT NULL
+	DROP TABLE dim.DimCustomer;
+
 CREATE TABLE dim.DimCustomer (
 	CustomerKey  INT IDENTITY(1,1) PRIMARY KEY,
-  CustomerID   NVARCHAR(10) NOT NULL UNIQUE,
-  FirstName    NVARCHAR(50) NULL,
-  LastName     NVARCHAR(50) NULL,
-  Email        NVARCHAR(100) NULL,
+    CustomerID   NVARCHAR(10) NOT NULL UNIQUE,
+    FirstName    NVARCHAR(50) NULL,
+    LastName     NVARCHAR(50) NULL,
+    Email        NVARCHAR(100) NULL,
 	GeographyKey INT NOT NULL REFERENCES dim.DimGeography(GeographyKey)
  
 );
 GO
 
 ---- Create DimGeography Table
+PRINT'>> Droping Table: dim.DimGeography'
+IF OBJECT_ID('dim.DimGeography ', 'U') IS NOT NULL
+	DROP TABLE dim.DimGeography;
+
 CREATE TABLE dim.DimGeography (
     GeographyKey INT IDENTITY(1,1) PRIMARY KEY,
     ZipCode      NVARCHAR(10) NULL,
@@ -116,13 +143,17 @@ CREATE TABLE dim.DimGeography (
     State        NVARCHAR(5) NULL,
     Region       NVARCHAR(20) NULL,
     District     NVARCHAR(20) NULL,
-	  Country NVARCHAR(20) NULL,
+	[Country] NVARCHAR(20) NULL,
     CONSTRAINT UQ_DimGeography UNIQUE (ZipCode, City, State, Region, District)
 );
 GO
 
 ---- Create Fact Table
-CREATE OR ALTER TABLE fact.FactSales (
+PRINT'>> Droping Table: fact.FactSales'
+IF OBJECT_ID('fact.FactSales', 'U') IS NOT NULL
+	DROP TABLE fact.FactSales;
+
+CREATE TABLE fact.FactSales (
     SalesKey        INT IDENTITY(1,1) PRIMARY KEY,
     DateKey         INT  NOT NULL REFERENCES dim.DimDate(DateKey),
     ProductKey      INT  NOT NULL REFERENCES dim.DimProduct(ProductKey),
@@ -133,3 +164,4 @@ CREATE OR ALTER TABLE fact.FactSales (
     dwh_create_date DATETIME2      NULL DEFAULT GETDATE()
 );
 GO
+
